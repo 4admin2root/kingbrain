7dc7bdd9d5327c7261acbcc93f3e45b6154646cd
4admin2root <zjlv@thoughtworks.com>
2018-01-11 19:23:05 +0800

    [zjlv] change port for jenkins

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 -p 50000:50000 -v /Users/zjlv/kingbrain/jenkins/data:/var/jenkins_home jenkins:2.60.3
+docker run -d -p 8082:8080 -p 50000:50000 -v /Users/zjlv/kingbrain/jenkins/data:/var/jenkins_home jenkins:2.60.3
